6949dd95ed52e3847cedf25c0a28c9fe0c5e3918
towerchen <towerchen@tencent.com>
2022-04-20 10:52:30 +0800

    fix: change suffix when fix
    --bug=0

--- commit.sh
+++ commit.sh
@@ -2,19 +2,27 @@
 
 # Commit and make a suffix message
 
+re='^[0-9]+$'
+fixkeyword='fix:'
+suffix='story'
+
 current_branch() {
   git symbolic-ref --short HEAD
 }
 
 saved_branch=$(current_branch)
-storyid="${saved_branch##*_}"
-re='^[0-9]+$'
+tapdid="${saved_branch##*_}"
 
-# if storyid is not a number , then set to 0
-if ! [[ $storyid =~ $re ]] ; then
-   storyid="0"
+# if tapdid is not a number , then set to 0
+if ! [[ $tapdid =~ $re ]] ; then
+    tapdid="0"
 fi
 
-git commit -m "$1" -m "--story=${storyid}" $2
+# if msg has fix keyword , then change to bug
+if [[ $1 =~ $fixkeyword]] ; then
+    suffix="bug"
+fi
 
-echo "commit ${saved_branch} with storyid:${storyid} done"
+git commit -m "$1" -m "--${suffix}=${tapdid}" $2
+
+echo "commit ${saved_branch} with ${suffix} ${tapdid} done"
