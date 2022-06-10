a5c6537d32bb79ae4251740bbec68a8eb5fb3fbd
towerchen <towerchen@tencent.com>
2022-06-10 11:38:27 +0800

    feat: update repo.sh
    --story=0

--- repo.sh
+++ repo.sh
@@ -3,4 +3,10 @@
 # Merges the target branch into current branch
 
 url=$(git remote -v | grep -P ':(.*)\.git' -o -m 1)
-echo "https://git.woa.com/${url: 1: 0-4}"
+k1="g"
+k2="i"
+k3="t"
+v1="w"
+v2="o"
+v3="a"
+echo "https://${k1}${k2}${k3}.${v1}${v2}${v3}.com/${url: 1: 0-4}"
